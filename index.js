55f182a452f2c1be80ce94c53c6b90fa0303979b
Tlenc <tlenciauskas@gmail.com>
2022-02-02 00:36:03 +0000

    testing routes

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-var http = require("http");
+const express = require("express");
+const http = require("http");
+const app = express();
 
-function requesthandler( req, res){
-    console.log("Req to : " + req.url);
-    res.end("Hello");
-}
+var path = require("path");
+var fs = require("fs");
 
-var server = http.createServer(requesthandler);
-server.listen(3000);
+app.get("/test", function(req,res){
+    res.send("Welcome to routing test");
+});
+
+app.get("/user/:userid", function(req,res){
+    var userId = parseInt(req.params.userid,10)
+    console.log("User id is : " + userId);
+    res.send("Welcom user " + userId);
+})
+
+app.use(function(req,res){
+    res.status(404).send("Page not found");
+})
+
+
+app.listen(3000);
